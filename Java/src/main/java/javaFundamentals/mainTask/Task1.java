63a5b5ae6895a9d3cd85b07ce7acd3f51edbeeb4
iryna_kavaliuk <iryna_kavaliuk@epam.com>
2021-03-15 17:44:40 +0300

    Main task

--- Java/src/main/java/javaFundamentals/mainTask/Task1.java
+++ Java/src/main/java/javaFundamentals/mainTask/Task1.java
@@ -1,4 +1,18 @@
 package javaFundamentals.mainTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task1 {
+//Приветствовать любого пользователя при вводе его имени через командную строку.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Как Ваше имя? ");
+        String name;
+        name = reader.readLine();
+        System.out.println("Добро пожаловать, " + name + "!");
+    }
+
 }
